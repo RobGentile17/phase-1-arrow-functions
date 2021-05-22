5a04b480e819dc8625e5f9f47d39a66982de2356
Rob <robert.gentile@powerhrg.com>
2021-05-22 15:24:53 -0400

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+let divide = function () {
+    return 2000/100
+}
+let square = function () {
+let x = 2
+return x * 2
+}
+let add = function () {
+    let a = 3
+    let b = 4
+    return a + b
+}
